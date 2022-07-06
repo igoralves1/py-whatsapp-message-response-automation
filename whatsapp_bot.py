63d1db0526f3869248a50696dab03401d40180bf
igoralves1 <igoralves1@gmail.com>
2022-07-06 18:20:05 -0400

    new env

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,9 +1,9 @@
 import pyautogui as pt
 import paperclip as pc
-from pynput.mouse import Controller, Button
+# from pynput.mouse import Controller, Button
 from time import sleep
 
-mouse = Controller()
+# mouse = Controller()
 
 class WhatsApp:
     def __init__(self, speed=5, click_speed=.3):
@@ -14,9 +14,13 @@ class WhatsApp:
 
     def nav_green_dot(self):
         try:
-            position = pt.locateOnScreen('green_pic.png', confidence = .7)
-            pt.moveTo(position[0:2], duration = self.speed)
-            pt.moveRel(-100, 0, duration = self.speed)
+            # position = pt.locateOnScreen('clip_pic.png', confidence = .7)
+            # position = pt.locateOnScreen('clip_pic.png')
+            # print(position)
+            print(pt.locateOnScreen('tt2.png'))
+            
+            # pt.moveTo(position[0:2], duration = self.speed)
+            # pt.moveRel(-100, 0, duration = self.speed)
         except Exception as e:
             print ('Exception (nav_green_dot): ', e)
 
